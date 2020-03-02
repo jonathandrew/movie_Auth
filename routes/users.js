073284957d1d077cab5805e8ee0ca3d2e4947d29
Jonathan Drew <jonathan.drew@codeimmersives.com>
2020-03-02 09:18:47 -0500

    final result gave up

--- routes/users.js
+++ routes/users.js
@@ -7,10 +7,26 @@ const Users = require("../models/Users");
 router.get("/register", (req, res) => {
   res.render("register");
 });
-
-router.get("/registered", (req, res) => {
-  res.render("registered", { user: new Users() });
+router.post("/register", (req, res) => {
+  const newUser = new Users();
+  const hashed = bcrypt.hash(req.body.password, 10);
+  newUser.name = req.body.name;
+  newUser.email = req.body.email;
+  newUser.password = req.body.password;
+  newUser
+    .save()
+    .then(user => {
+      res.status(200).json({
+        message: " user is created with nonecrypted password fudge",
+        user
+      });
+      return res.redirect("/users/login");
+    })
+    .catch(err => {
+      return res.status(400).json({ message: "user is created already", err });
+    });
 });
+
 router.get("/login", (req, res) => {
   res.render("login");
 });
